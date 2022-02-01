63a6a4a3880918bf33d2cd5154d69077922d184d
hen1227 <henhen1227@gmail.com>
2022-02-01 11:05:25 -0500

    Server Side First Commit
    Replaced the placeholder website with the server side code to handle and store the busses route

---
+++ website/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>SPS Bus Tracker!</title>
+<link rel="shortcut icon" type="image/png" href="favicon.png"/>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="header.js"></script>
+<link rel="stylesheet" href="/index.css">
+
+<style>
+body {
+    background: #E8CBC0;
+    background: -webkit-linear-gradient(to right, #E8CBC0, #636FA4);
+    background: linear-gradient(to right, #E8CBC0, #636FA4);
+    min-height: 100vh;
+    overflow: auto;
+}
+
+.text-gray {
+    color: #999;
+}
+</style>
+<div w3-include-html="header.html"></div>
+</head>
+<body>
+
+<center>
+	
+<br>
+	<h3>SPS Bus Tracker</h3>
+	<br>
+	<br>
+	<p>Track the bus!</p>
+	<br>
+	<br>
+	<a href="route.json">
+	<button style="background-color:rgba(70%,70%,70%,50%);  padding:15px;">Bus route</button>
+	</a>
+	
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<a href="/">
+	<button style="background-color:rgba(70%,70%,70%,50%);  padding:10px 55px;">Back</button>
+	</a>
+	<br>
+	
+</center>
+<script>
+includeHTML();
+</script>
+</body>
+</html>
---
+++ website/upload.php
@@ -0,0 +1,27 @@
+<?php
+$newCoordsArray = explode(",",$_POST['loc']);
+// $newCoords = [
+	// $_POST['time'] => [
+		// floatval($newCoordsArray[0]),
+		// floatval($newCoordsArray[1])
+	// ]
+// ];
+
+$url = 'route.json';
+
+$pastCoords = json_decode(file_get_contents($url), TRUE);
+
+// array_push($pastCoords[0], $newCoords);
+$pastCoords[$_POST['time']] = [
+		floatval($newCoordsArray[0]),
+		floatval($newCoordsArray[1])
+];
+	
+$json = json_encode($pastCoords, JSON_PRETTY_PRINT);
+
+$file = fopen($url,'w');
+fwrite($file, $json);
+fclose($file);
+		  
+echo 'Added'.$json ;
+?>
