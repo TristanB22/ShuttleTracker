f7c128ef1efc72092d0813955a78f3ba58d5d0b9
Henry Abrahamsen <32469043+hen1227@users.noreply.github.com>
2022-02-01 11:08:28 -0500

    Update index.php

--- website/index.php
+++ website/index.php
@@ -7,8 +7,6 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="header.js"></script>
-<link rel="stylesheet" href="/index.css">
 
 <style>
 body {
@@ -23,13 +21,12 @@ body {
     color: #999;
 }
 </style>
-<div w3-include-html="header.html"></div>
 </head>
 <body>
 
-<center>
+    <center>
 	
-<br>
+	<br>
 	<h3>SPS Bus Tracker</h3>
 	<br>
 	<br>
